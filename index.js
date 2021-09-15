f20bb66df676c728928023f70053e7905cde674b
urgensonamlama <urgensonamlama@gmail.com>
2021-09-15 14:48:14 -0400

    commit tree 77c34d512e9171b51cd3caa96261fa22a6f94789

--- index.js
+++ index.js
@@ -1,11 +1,44 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(someValue){
+  let result;
+  if (someValue <= 400){
+    result = 'This one is on me!';
+  }
+  else if (someValue >=2000 && someValue <= 2500){
+    result = 'I will gladly take your thirty bucks.'
+  }
+  else if (someValue > 2500){
+    result = 'No can do.';
+  }
+  return result;
   // Write your code here!
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(someValue){
+  let result;
+
+  someValue === 'NYC' ? result = "Ok, sounds good." : result= "No go."
+
+  return result;
+
   // Write your code here!
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(someValue){
   // Write your code here!
+
+let result;
+
+switch(someValue){
+  case 'generous':
+    result = "Thank you so much."
+    break;
+  case 'not as generous':
+    result = "Thank you."
+    break;
+  default:
+    result = "Bye."
+    break;
+}
+return result;
+
 }
